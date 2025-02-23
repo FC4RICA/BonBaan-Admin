cefc40ce005c7844b1b4ba90050a34f674033d47
FC4RICA <kantinan.fw@gmail.com>
2025-02-23 15:57:29 +0700

    fix: move loader and action to route

---
+++ src/routes/loginRoute.jsx
@@ -0,0 +1,33 @@
+import { useActionData, useLoaderData } from "react-router";
+import { login } from "../api/userApi";
+
+export const loginLoader = async () => {
+  try {
+    const token = sessionStorage.getItem("token")
+    return { token }
+  } catch (error) {
+    return null
+  }
+}
+
+export const loginAction = async ({ request }) => {
+  const data = await request.formData();
+  const body = {
+    emailOrUsername: data.get("username"),
+    password: data.get("password")
+  }
+
+  try {
+    const response = await login(body);
+    return response
+  } catch (error) {
+    return { error: "Invalid credentials" };
+  }
+}
+
+export const useLogin = () => {
+  const loaderData = useLoaderData();
+  const actionData = useActionData();
+
+  return actionData || loaderData;
+}
